eec34f6c29564553da713b9ecdda84149f89e7f7
xpsylon <xxxpsylon@gmail.com>
2023-02-01 15:36:18 +0100

    20230201 dics

--- diccionarios_AR_EX_3.py
+++ diccionarios_AR_EX_3.py
@@ -9,25 +9,81 @@
 ##Socio no2, Bárbara Molina, ingresó: 17/03/2009, cuota al día.
 ##Socio no3, Lautaro Campos, ingresó: 17/03/2009, cuota al día.
 socios = {1:['Amanda Nunez', '17/03/2009', True], 2:['Barbara Molina', '17/03/2009',
-True], 3:['Lautaro Campos', '17/03/2009', True]}
+True], 3:['Lautaro Campos', '17/03/2009', True], 4:['Maria Galeri', '13/03/2018', False],
+5:['Estela Campos', '13/03/2018', True]}
 for i,j in socios.items():
     print('#', i, j)
+
+print('AGREGAR NUEVO SOCIO')    
 numero = int(input('Numero de socio: '))
-nombre = input('Nombre de socio: ')
-fecha_ingreso = input('Fecha ingreso dd/mm/yyyy: ')
-cuotas_ok = input('Cuota al dia?...SI NO: ')
-if cuotas_ok == 'si':
-    cuotas_ok = True
-else:
-    cuotas_ok = False
+while numero != 0:
+    nombre = input('Nombre de socio: ')
+    fecha_ingreso = input('Fecha ingreso dd/mm/yyyy: ')
+    cuotas_ok = input('Cuota al dia?...SI NO: ')
+    if cuotas_ok == 'si':
+        cuotas_ok = True
+    else:
+        cuotas_ok = False
+    #agrega socio usando numero como clave y las otras variables entran como lista en valor
+    socios[numero] = [nombre, fecha_ingreso, cuotas_ok]
+    numero = int(input('Otro numero de socio: '))
 
-socios[numero] = [nombre, fecha_ingreso, cuotas_ok]
 ##Informar cantidad de socios del club.
+print('Cantidad de socios:',len(socios))
 
-##Solicitar al usuario el número de un socio y registrar que ha pagado todas las cuotas adeudadas.
+##Solicitar al usuario el número de un socio y registrar que ha pagado todas
+#las cuotas adeudadas.
+print('ESTADO DE CUOTAS')
+numero = int(input('Numero de socio: '))
+while numero != 0:
+    if socios[numero][2] == True:
+        print('Cuotas al dia')
+    else:
+        print('El socio adeuda cuotas')
+    numero = int(input('Otro numero de socio: '))
 
-##Modificar la fecha de ingreso de todos los socios ingresados el 13/03/2018, para indicar que en realidad ingresaron el 14/03/2018.
+##Modificar la fecha de ingreso de todos los socios ingresados el 13/03/2018,
+#para indicar que en realidad ingresaron el 14/03/2018.
+print('MODIFICAR FECHA DE INGRESO')
+for valor in socios.values():
+    if valor[1] == '13/03/2018':
+        valor[1] = '14/03/2018'
 
-##Solicitar el nombre y apellido de un socio y darlo de baja (eliminarlo del listado).
+##Solicitar el nombre y apellido de un socio y darlo de baja
+#(eliminarlo del listado). del diccionario[clave]
+nombre = input('Nombre de socio a dar de baja: ')
+for i in socios:
+    if nombre == socios[i][0]:
+        del socios[i]
+        
+
+        
 
 ##Imprimir el listado de socios completo.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
